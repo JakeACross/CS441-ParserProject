034106a22253650134dc53ab884c0b4abae2b650
JakeACross <jackgw@mail.umkc.edu>
2021-10-18 16:01:36 -0500

    Small Updates

--- parser.rkt
+++ parser.rkt
@@ -1,37 +1,38 @@
 #lang racket
 
 (require racket/file)
-(provide file->list)
 
-
-
-;;(define in (open-input-file "Input01.txt"))
-;;(read-line in)
-;;(close-input-port in)
 
 (define (parser some-file)
 (define templist (list "stmt" "stmtlist"))
 (define inline(file->lines some-file))
+;local function llp:
+  (define (llp lst len)
+    (cond
+     [(list-prefix? '("$$ ") lst) "Accept"]
+     [else
+      (println len)
 
-(for-each (lambda (arg)
-              (printf "Got ~a\n" arg)
-              23)
-            inline)
-
-(define firstitem (first inline))
-(set! inline(remove firstitem inline))
-(display inline)
-(display "\n")
-
-(define item(list-ref inline 0))
-(define pusheditem(string-split item))
-(display item)
-(append pusheditem templist))
+      (llp (rest lst) (+ len 1))
 
 
 
-;;function call here
+
+
+           ]))
+
+
+
+  
+  ;body of parser calls llp:
+  (llp inline 0))
+
+
+
+
+;function call here
 (parser "Input01.txt")
+
 
 
 
