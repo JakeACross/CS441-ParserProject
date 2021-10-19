e6e9b9a93cebbfdad77ea1b32417f95f672d9831
JakeACross <jackgw@mail.umkc.edu>
2021-10-19 02:03:28 -0500

    Add input files

--- parser.rkt
+++ parser.rkt
@@ -4,35 +4,44 @@
 
 
 (define (parser some-file)
-(define templist (list "stmt" "stmtlist"))
+(define stack (list "stmt" "stmtlist"))
 (define inline(file->lines some-file))
 ;local function llp:
   (define (llp lst len)
     (cond
      [(list-prefix? '("$$ ") lst) "Accept"]
      [else
-      (println len)
-
-      (llp (rest lst) (+ len 1))
-
-
-
-
-
-           ]))
+      (define input (string-split (first lst)))
+      (println input)
+      (cond
+        [(list-prefix? '("read") input)
+         (set! stack (append(list "read" "id") stack))
+         (if (< (length input) 3) (llp (rest lst) (+ len 1)) (error "Syntax error found on line " (~a len)))
+         ]
+        [(list-prefix? '("write") input)
+         (set! stack (append(list "write" "expr") stack))
+         (llp (rest lst) (+ len 1))
+         ]
+        [else  (llp (rest lst) (+ len 1))]
+        )
+      ]))
 
 
 
   
   ;body of parser calls llp:
-  (llp inline 0))
+  (llp inline 1))
 
 
 
 
 ;function call here
-(parser "Input01.txt")
+(parser "Input03.txt")
 
+
+;(define inlist(file->list "Input01.txt"))
+;(for ([x inlist])
+;(println x))
 
 
 
